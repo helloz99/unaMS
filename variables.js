d5b758bd4940f99a7edf2fdc2954dc98bd052b0b
helloz <eloroesverde@gmail.com>
2023-02-07 14:50:31 -0700

    ready

---
+++ variables.js
@@ -0,0 +1,27 @@
+//string 
+
+var bootcamp = 'henry';
+
+//numeros
+
+var numeroEntero = 17;
+
+var negativeNumber = -4;
+
+var Decimal = 11.75;
+
+//booleans
+
+var imLearning = true;
+
+var imBored = false;
+
+//undefined
+
+var cajon1;
+
+console.log(cajon1)
+
+//Null
+
+var Sinnada = null
